4a79edc36f934ac7139e84b56ecb6de05f942b85
PranviComputech <pranvicomputech@gmail.com>
2023-12-26 17:12:26 +0530

    UI update calculation bug fixes

--- script.js
+++ script.js
@@ -10,31 +10,31 @@ let currentDate = `${day}-${month}-${year} ${dayName}`;
 document.getElementById('today').innerHTML = currentDate
 checkUndefined()
 function checkUndefined() {
-        document.getElementById('total1').value = 0
-        document.getElementById('total2').value = 0
-        document.getElementById('total3').value = 0
-        document.getElementById('color1').value = 0
-        document.getElementById('color2').value = 0
-        document.getElementById('color3').value = 0
-        document.getElementById('raw1').value = 0
-        document.getElementById('raw2').value = 0
-        document.getElementById('raw3').value = 0
-        document.getElementById('rate1').value = 0
-        document.getElementById('rate2').value = 0
-        document.getElementById('rate3').value = 0
+    document.getElementById('total1').value = 0
+    document.getElementById('total2').value = 0
+    document.getElementById('total3').value = 0
+    document.getElementById('color1').value = 0
+    document.getElementById('color2').value = 0
+    document.getElementById('color3').value = 0
+    document.getElementById('raw1').value = 0
+    document.getElementById('raw2').value = 0
+    document.getElementById('raw3').value = 0
+    document.getElementById('rate1').value = 0
+    document.getElementById('rate2').value = 0
+    document.getElementById('rate3').value = 0
 }
 
-function calcraw1() {    
+function calcraw1() {
     let color1 = document.getElementById('color1').value
-    if(color1 == '')
-    document.getElementById('color1').value = 0
+    if (color1 == '')
+        document.getElementById('color1').value = 0
     if (color1 == undefined)
         color1 = 0
-    document.getElementById('raw1').value = (color1 / .75).toFixed(2)
+    document.getElementById('raw1').value = (color1 / .75).toFixed(3)
     calctotal1()
 }
 
-function calctotal1() {    
+function calctotal1() {
     let colorring = 0
     let color1 = document.getElementById('color1').value
     let raw1 = document.getElementById('raw1').value
@@ -46,7 +46,7 @@ function calctotal1() {
         colorring = 0
     }
     else
-        colorring = raw1 * clbr
+        colorring = Math.ceil(raw1 * clbr)    
     let total = raw1 * rate1 + colorring
     total = total.toFixed()
     document.getElementById('total1').value = total
@@ -55,17 +55,17 @@ function calctotal1() {
 }
 
 /////////////////////////////////
-function calcraw2() {    
+function calcraw2() {
     let color2 = document.getElementById('color2').value
-    if(color2 == '')
-    document.getElementById('color2').value = 0
+    if (color2 == '')
+        document.getElementById('color2').value = 0
     if (color2 == undefined)
         color2 = 0
-    document.getElementById('raw2').value = (color2 / .75).toFixed(2)
+    document.getElementById('raw2').value = (color2 / .75).toFixed(3)
     calctotal2()
 }
 
-function calctotal2() {    
+function calctotal2() {
     let colorring = 0
     let color2 = document.getElementById('color2').value
     let raw2 = document.getElementById('raw2').value
@@ -77,7 +77,7 @@ function calctotal2() {
         colorring = 0
     }
     else
-        colorring = raw2 * clbr
+        colorring = Math.ceil(raw2 * clbr)
     let total = raw2 * rate2 + colorring
     total = total.toFixed()
     document.getElementById('total2').value = total
@@ -86,17 +86,17 @@ function calctotal2() {
 }
 
 /////////////////////////////////
-function calcraw3() {    
+function calcraw3() {
     let color3 = document.getElementById('color3').value
-    if(color3 == '')
-    document.getElementById('color3').value = 0
+    if (color3 == '')
+        document.getElementById('color3').value = 0
     if (color3 == undefined)
         color3 = 0
-    document.getElementById('raw3').value = (color3 / .75).toFixed(2)
+    document.getElementById('raw3').value = (color3 / .75).toFixed(3)
     calctotal3()
 }
 
-function calctotal3() {    
+function calctotal3() {
     let colorring = 0
     let color3 = document.getElementById('color3').value
     let raw3 = document.getElementById('raw3').value
@@ -108,7 +108,7 @@ function calctotal3() {
         colorring = 0
     }
     else
-        colorring = raw3 * clbr
+        colorring = Math.ceil(raw3 * clbr)
     let total = raw3 * rate3 + colorring
     total = total.toFixed()
     document.getElementById('total3').value = total
